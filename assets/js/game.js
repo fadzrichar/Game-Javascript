091230b4b4b357ec040e9301c66244de33eb3c4c
bimon-alta <bimon@alterra.id>
2019-12-26 21:04:32 +0700

    menambahkan musuh

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,9 @@
-const score = document.querySelector(".score");
-const startScreen = document.querySelector(".startScreen");
-const gameArea = document.querySelector('gameArea')
+// const score = document.querySelector(".score");
+// const startScreen = document.querySelector(".startScreen");
+// const gameArea = document.querySelector(".gameArea");
+const score = document.getElementsByClassName("score")[0];
+const startScreen = document.getElementsByClassName("startScreen")[0];
+const gameArea = document.getElementsByClassName("gameArea")[0];
 let player = {
     speed: 5
     , score: 0
@@ -17,6 +20,8 @@ document.addEventListener("keyup", pressOff);
 
 function moveLines() {
     let lines = document.querySelectorAll(".line");
+    // let lines = document.getElementsByClassName("line");
+
     lines.forEach(function (item) {
         if (item.y >= 1500) {
             item.y -= 1500;
@@ -50,16 +55,36 @@ function moveEnemy(car) {
     })
 }
 
+function gerakTurunKoin(koin) {
+    let ele = document.querySelectorAll(".koin");
+    ele.forEach(function (item) {
+        // if (isCollide(car, item)) {
+        //     console.log("HIT");
+        //     endGame();
+        // }
+        if (item.y >= 1500) {
+            item.y = -600;
+            item.style.left = Math.floor(Math.random() * 350) + "px";
+            item.style.backgroundColor = randomColor();
+        }
+        item.y += player.speed;
+        item.style.top = item.y + "px";
+    })
+}
+
 function playGame() {
-    let car = document.querySelector(".car");
     moveLines();
+    let car = document.querySelector(".car");
     moveEnemy(car);
+
+    let koin = document.querySelector(".koin");
+    gerakTurunKoin(koin);
     let road = gameArea.getBoundingClientRect();
     if (player.start) {
         if (keys.ArrowUp && player.y > road.top) {
             player.y -= player.speed;
         }
-        if (keys.ArrowDown && player.y < road.bottom) {
+        if (keys.ArrowDown && player.y < road.bottom - 200) {
             player.y += player.speed;
         }
         if (keys.ArrowLeft && player.x > 0) {
@@ -72,7 +97,8 @@ function playGame() {
         car.style.top = player.y + 'px';
         window.requestAnimationFrame(playGame);
         player.score++;
-        score.innerText = "Score: " + player.score;
+        score.innerText = "Score: " + player.score ;
+        
     }
 }
 
@@ -113,6 +139,7 @@ function start() {
     player.x = car.offsetLeft;
     player.y = car.offsetTop;
     for (let x = 0; x < 3; x++) {
+        //ciptain 3 musuh setiap render 
         let enemy = document.createElement("div");
         enemy.classList.add("enemy");
         enemy.innerHTML = "<br>" + (x + 1);
@@ -122,6 +149,16 @@ function start() {
         enemy.style.backgroundColor = randomColor();
         gameArea.appendChild(enemy);
     }
+
+    //ciptain 1 koin setiap render
+    let koin = document.createElement("div");
+    koin.classList.add("koin");
+    koin.innerHTML = "<br> $";
+    koin.y = ((2 + 1) * 600) * -1;
+    koin.style.top = koin.y + "px";
+    koin.style.left = Math.floor(Math.random() * 350) + "px";
+    enemy.style.backgroundColor = randomColor();
+    gameArea.appendChild(koin)
 }
 
 function randomColor() {
